5308d3fe48aff131d8bd0a642514c19e8fe04539
Moshe <mobgadol@gmail.com>
2022-06-10 13:51:55 -0400

    added error handling, displaying users info to page, loop to display user repo info

--- homepage.js
+++ homepage.js
@@ -1,19 +1,84 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
 
-// How to search for specific parameters within an API:
-
-// create the function with whatever parameter you want, in this case username
 var getUserRepos = function(user) {
-    // format the github api url and put whatever params you want it to add in the middle of the strings
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
   
-    // make a request to the url and tell it to format in a certain way, in this case JSON
-    fetch(apiUrl).then(function(response) {
-        // once it is formatted in JSON...
+    fetch(apiUrl)
+  .then(function(response) {
+    // request was successful
+    if (response.ok) {
       response.json().then(function(data) {
-        //   console log whatever data is collected so it can be seen
-        console.log(data);
+        displayRepos(data, user);
       });
-    });
-  };
-//   when calling the function, add the name username you want the browser to fetch the repos of, and VOILA......
-  getUserRepos("ukraine");
+    } else {
+        alert('Error: GitHub User Not Found');
+       }
+  })
+  .catch(function(error) {
+    // Notice this `.catch()` getting chained onto the end of the `.then()` method
+    alert("Unable to connect to GitHub");
+  });
+
+};
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    // get value from input element
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+    getUserRepos(username);
+    nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+};
+
+var displayRepos = function(repos, searchTerm) {
+    // check if api returned any repos
+    if (repos.length === 0) {
+        repoContainerEl.textContent = "No repositories found.";
+        return;
+    }
+  
+    repoContainerEl.textContent = "";
+    repoSearchTerm.textContent = searchTerm;
+    // loop over repos
+    for (var i = 0; i < repos.length; i++) {
+        // format repo name
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
+    
+        // create a container for each repo
+        var repoEl = document.createElement("div");
+        repoEl.classList = "list-item flex-row justify-space-between align-center";
+    
+        // create a span element to hold repository name
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+    
+        // append to container
+        repoEl.appendChild(titleEl);
+
+        // create a status element
+        var statusEl = document.createElement("span");
+        statusEl.classList = "flex-row align-center";
+
+        // check if current repo has issues or not
+        if (repos[i].open_issues_count > 0) {
+            statusEl.innerHTML =
+            "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+        } else {
+                statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+           }
+
+        // append to container
+        repoEl.appendChild(statusEl);
+        // append container to the dom
+        repoContainerEl.appendChild(repoEl);
+    }
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
