30b7ca83af824675b2dfe45587f285e1e9b482b7
Moshe <mobgadol@gmail.com>
2022-06-10 12:56:03 -0400

    call specific parameters from the API and return it within a string to get the desired search results

--- homepage.js
+++ homepage.js
@@ -1,5 +1,19 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
-  };
+
+// How to search for specific parameters within an API:
+
+// create the function with whatever parameter you want, in this case username
+var getUserRepos = function(user) {
+    // format the github api url and put whatever params you want it to add in the middle of the strings
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
   
-  getUserRepos();
+    // make a request to the url and tell it to format in a certain way, in this case JSON
+    fetch(apiUrl).then(function(response) {
+        // once it is formatted in JSON...
+      response.json().then(function(data) {
+        //   console log whatever data is collected so it can be seen
+        console.log(data);
+      });
+    });
+  };
+//   when calling the function, add the name username you want the browser to fetch the repos of, and VOILA......
+  getUserRepos("ukraine");
